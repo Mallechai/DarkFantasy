09ea4961c2917f88139952fc4d66ea1486334441
Stefan Geers <stefangeers@gmail.com>
2016-04-05 16:41:53 +0200

    Player class

---
+++ src/darkfantasy/Player.java
@@ -0,0 +1,18 @@
+package darkfantasy;
+
+public class Player {
+	private String name;
+	//private static boolean playerpresent = false;
+	
+	public Player(String name){
+		this.name = name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	public String getName(){
+		return this.name;
+	}	
+}
