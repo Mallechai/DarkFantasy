400421ed3a3c819e901b1c035c96eea94b89536f
Stefan Geers <stefangeers@gmail.com>
2016-04-05 17:01:44 +0200

    meer player stuff

--- src/darkfantasy/Player.java
+++ src/darkfantasy/Player.java
@@ -8,9 +8,9 @@ public class Player {
 		this.name = name;
 	}
 
-	public void setName(String name) {
+	/*public void setName(String name) {
 		this.name = name;
-	}
+	}*/
 	
 	public String getName(){
 		return this.name;
