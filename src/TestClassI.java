ed75dca2b1a7561e1ef7b1a4ba8c5655c9a76682
jwpwo <jwpwo@10.2.8.154>
2016-04-05 16:03:47 +0200

    Thus we begin

--- src/TestClassI.java
+++
@@ -1,7 +0,0 @@
-
-public class TestClassI {
-	public static void main(String[] args) {
-		System.out.println("Woohoo!");
-	}
-
-}
---
+++ src/darkfantasy/IntroductionStory.java
@@ -0,0 +1,10 @@
+package darkfantasy;
+
+public class IntroductionStory {
+
+	public static void main(String[] args) {
+		System.out.println("Thus the story begins");
+
+	}
+
+}
