08b61f699e2c61c66bd8d17935f1ad36341a42b3
Stefan Geers <stefangeers@gmail.com>
2016-04-08 15:06:14 +0200

    Project als dynamic web project

---
+++ src/javaclasses/Player.java
@@ -0,0 +1,18 @@
+package javaclasses;
+
+public class Player {
+	private String name;
+	//private static boolean playerpresent = false;
+	
+	public Player(String name){
+		this.name = name;
+	}
+
+	/*public void setName(String name) {
+		this.name = name;
+	}*/
+	
+	public String getName(){
+		return this.name;
+	}	
+}
